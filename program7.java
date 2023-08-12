a9cb50ffea317c04f22de62b17e3f0d9c0e6780f
sekar N <sekarnagarajan1234@gmail.com>
2023-08-12 19:59:42 +0530

    java

---
+++ program7.java
@@ -0,0 +1,11 @@
+//7. Reverse the array
+ class program7 {
+    public static void main(String[] args) {
+        int [] num = {1,2,3,4,5};
+        System.out.print("Array Reverse :");
+        for (int i = num.length-1;i>=0;i--){
+            System.out.print(num[i] + " ");
+        }
+
+    }
+}
