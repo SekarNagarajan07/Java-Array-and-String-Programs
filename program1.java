661e8613598ee93bd18984597b7d0000845b352d
sekar N <sekarnagarajan1234@gmail.com>
2023-08-12 19:04:57 +0530

    java

--- program1.java
+++ program1.java
@@ -1,6 +1,13 @@
 //1.Find the duplicate numbers from an array?
 class program1 {
   public static void main(String[] args) {
-
+    int [] num = {2,9,5,6,7,5,4};
+    for (int i = 0;i<num.length;i++){
+      for (int j = 0;j<i;j++){
+        if(num[i] == num[j]) {
+          System.out.println("The Duplicate number is :" + num[j]);
+        }
+      }
+    }
   }
 }
