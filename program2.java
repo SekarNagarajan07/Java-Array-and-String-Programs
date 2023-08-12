4b6aa8cfa1d9451eeecb0c1518fadbdf842d3204
sekar N <sekarnagarajan1234@gmail.com>
2023-08-12 19:23:50 +0530

    java

--- program2.java
+++ program2.java
@@ -1,2 +1,20 @@
-public class program2 {
+// 2.Find the Largest Number from an array?
+ class program2 {
+    public static void main(String[] args) {
+        int [] num = {1,2,3,7,6};
+        int max,size;
+        size = num.length;
+        for(int i = 0;i<size;i++){
+            for (int j = i + 1;j<size;j++){
+                if(num[i] > num[j]){
+                   max = num[i];
+                   num[i] = num[j];
+                   num[j] = max;
+                }
+            }
+        }
+        System.out.println("Largest Number is :" + num[size-1]);
+
+    }
+
 }
