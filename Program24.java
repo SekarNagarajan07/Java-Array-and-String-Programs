16c3d0863042bc8a8cce2ee7b968ee9277e8f8d0
sekar N <sekarnagarajan1234@gmail.com>
2023-08-16 15:37:49 +0530

    java

---
+++ Program24.java
@@ -0,0 +1,9 @@
+//24. Write the program to reverse the string
+class Program24 {
+    public static void main(String[] args) {
+        String name = "sekar";
+        for (int i = name.length()-1;i>=0;i--){
+            System.out.print(name.charAt(i) + " ");
+        }
+    }
+}
