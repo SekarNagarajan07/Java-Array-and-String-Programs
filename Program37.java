b741f24551c4cf325eed2bc90d83dcfce45fe72d
sekar N <sekarnagarajan1234@gmail.com>
2023-08-17 20:19:41 +0530

    java

---
+++ Program37.java
@@ -0,0 +1,7 @@
+//37. Java Program to print the number of elements present in an array
+class Program37 {
+    public static void main(String[] args) {
+        int [] a = new int[]{1,2,3,4,5};
+        System.out.println(a.length);
+    }
+}
