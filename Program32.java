7c8110ae5ebd3cb738d77818a6fc1237c9ccce27
sekar N <sekarnagarajan1234@gmail.com>
2023-08-17 19:43:00 +0530

    java

---
+++ Program32.java
@@ -0,0 +1,19 @@
+//32. How to print first non repeated character from String?
+class Program32 {
+    public static void main(String[] args) {
+        String name = "java";
+        char [] ch = name.toCharArray();
+        for (int i = 0;i<ch.length;i++){
+            int j;
+            for (j = 0;j<ch.length;j++){
+                if(ch[i]==ch[j] && i!=j){
+                    break;
+                }
+            }
+            if(j == ch.length){
+                System.out.println("first non repeated character from String: " + ch[i]);
+                break;
+            }
+        }
+    }
+}
